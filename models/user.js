e7b22247d5c4da592c7bef61eae059f4bf4818d7
Peter Kang <peeterkang@gmail.com>
2020-03-06 11:48:49 -0800

    model setup

---
+++ models/user.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+    let User = sequelize.define(
+      "User",
+      {
+        email: DataTypes.STRING,
+        full_name: DataTypes.STRING,
+        zipcode: DataTypes.INTEGER,
+        fitness_goal: DataTypes.STRING
+      },
+      {
+        timestamps: false
+      }
+    );
+    return User;
+  };
+  
