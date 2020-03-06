2f9a81beab9eb2a94bcc11fd3f944feecf85167d
Peter Kang <peeterkang@gmail.com>
2020-03-06 11:54:40 -0800

    skjdahsuagdah

--- models/user.js
+++ models/user.js
@@ -1,20 +1,4 @@
 module.exports = function(sequelize, DataTypes) {
-<<<<<<< HEAD
-  let User = sequelize.define(
-    "User",
-    {
-      email: DataTypes.STRING,
-      full_name: DataTypes.STRING,
-      zipcode: DataTypes.INTEGER,
-      fitness_goal: DataTypes.STRING
-    },
-    {
-      timestamps: false
-    }
-  );
-  return User;
-};
-=======
     let User = sequelize.define(
       "User",
       {
@@ -29,5 +13,3 @@ module.exports = function(sequelize, DataTypes) {
     );
     return User;
   };
-  
->>>>>>> a44d518b772d24d78a3e215a7e6301a3fde0526f
